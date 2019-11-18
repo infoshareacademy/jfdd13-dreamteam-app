fd7aab44f7336d2de9a0b378f926618a1dc27c4d
rednaxela700 <ohleg@windowslive.com>
2019-11-18 15:49:49 +0100

    basic header styling

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Icon} from "semantic-ui-react";
 
 function Appbar () {
+    const appbarStyles = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '5vh',
+        backgroundColor: '#1b1c1d',
+        fontSize: '1.5rem',
+        color: '#fff'
+    };
   return (
 
     <div className="Appbar">
-      
+
+      <h1 style={appbarStyles}>
+          <Icon name="paper plane outline"/>
+          WAY.TO
+      </h1>
     </div>);
 };
 
