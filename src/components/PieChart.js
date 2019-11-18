1986561f0f029e5ebbf0816419c51da87835fec6
rednaxela700 <ohleg@windowslive.com>
2019-11-18 16:27:28 +0100

    example piechart in PieChart.js

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import {Button} from "semantic-ui-react";
+import PieChart from "react-minimal-pie-chart";
 
 const pieChart = ()=> {
     console.log('PieChart message')
-}
+    return (
+        <PieChart
+            data={[
+                { title: 'One', value: 10, color: '#E38627' },
+                { title: 'Two', value: 15, color: '#C13C37' },
+                { title: 'Three', value: 20, color: '#6A2135' },
+            ]}
+        />
+    )
+};
 
 export default pieChart
