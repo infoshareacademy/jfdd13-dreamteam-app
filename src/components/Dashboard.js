4bf6c90efe46492dc4b9ec682aa5b239c26ad607
rednaxela700 <ohleg@windowslive.com>
2019-11-18 22:32:23 +0100

    deleted unused styles from Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,7 @@ import React from 'react';
 import PieChart from "./PieChart";
 
 function Dashboard () {
-    const chartStyles = {
-        height: '200px'
-    };
   return (
-
     <div className="Dashboard">
         <PieChart/>
     </div>
