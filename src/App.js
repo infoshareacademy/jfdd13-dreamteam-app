294e9f0feb9ffdf3de8b08a7928a6ac150d620ce
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-18 18:50:11 +0100

    Navbar is always on;  all components added to routing in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 import TripContainer from "./components/TripContainer";
 import Form from "./components/Form";
+import Search from "./components/Search";
+import Favourites from "./components/Favourites";
 import Appbar from "./components/Appbar";
 import 'semantic-ui-css/semantic.min.css'
 
@@ -13,14 +15,16 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Appbar />
-        {/* <Navbar />
-        <Dashboard />
+        <Navbar />
+        {/* <Dashboard />
         <TripContainer />
         <Form />         */}
         <Switch>
           <Route exact strict path="/" component={() => <h1>we are the dreamTeam and this is our App!</h1>} />
           <Route exact strict path="/main" component={Dashboard} />
-          <Route exact strict path="/trip" component={() => <h1>TripContainer</h1>} />
+          <Route exact strict path="/trip" component={TripContainer} />
+          <Route exact strict path="/search" component={Search} />
+          <Route exact strict path="/fav" component={Favourites} />
           <Route exact strict path="/form/" component={Form} />
           <Route component={() => <h1>404 - sadface</h1>} />
         </Switch>
--- src/components/TripContainer.js
+++ src/components/TripContainer.js
@@ -4,7 +4,7 @@ function TripContainer() {
   return (
   
     <div className="TripContainer">
-      <h1>TripContainer</h1>
+      <h1>we believe all of our amazing trips we'll be here </h1>
 
     </div> 
   );
