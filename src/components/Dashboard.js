a53d713c131696af5424eb7130a0c3484446cec1
rednaxela700 <ohleg@windowslive.com>
2019-11-19 16:11:29 +0100

    added grid to Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import PieChart from "./PieChart";
+import {Grid, Image} from "semantic-ui-react";
 
-function Dashboard () {
-  return (
-    <div className="Dashboard">
-        <PieChart/>
-    </div>
-  );
+function Dashboard() {
+    return (
+        <div className="Dashboard">
+            <Grid padded={true}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h2>Ptaki latają kluczem</h2>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered={true}>
+                    <Grid.Column width={8}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png'/>
+                        <PieChart/>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png'/>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered={true}>
+                    <Grid.Column width={8}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png'/>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png'/>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
+    )
 }
 
-export default Dashboard;
+
+export default Dashboard
