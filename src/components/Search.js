8b7b948dcff29ec73423f7d73c66cd057360888b
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-19 19:54:31 +0100

    Dropdown added with other changes to slider-Form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
-import { Form, Grid, Header, Input} from 'semantic-ui-react'
+import { Grid, Header, Form, Input, Dropdown } from 'semantic-ui-react'
+import TripContainer from './TripContainer'
 
+const continentsOptions = [
+  {key: 'afr', color: 'black', text: "Afryka"},
+  {key: 'apn', color: 'red', text: "Ameryka Północna"},
+  {key: 'apd', color: 'green', text: "Ameryka Południowa"},
+  {key: 'ant', color: 'white', text: "Antarktyda"},
+  {key: 'aus', color: 'blue', text: "Australia i Oceania"},
+  {key: 'eur', color: 'grey', text: "Europa"}
+]
 class Search extends Component {
-  state = {show: 7}
+  state = {show: 999}
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value })
   toggleVisibility = () =>
@@ -12,44 +21,50 @@ class Search extends Component {
     const {show} = this.state
 
     return (
-  
-    <div className="Search">
+      <div className="Search">
+        <Grid padded={true}>
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <Header>Wskaż kierunek!</Header>
+            </Grid.Column>
+          </Grid.Row>
 
-  <Grid padded={true}>
-    <Grid.Row columns={1}>
-      <Grid.Column>
-        <Header>Wskaż kierunek!</Header>
-      </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row columns={3}>
-      <Grid.Column>
-        <Input focus icon='search' placeholder='Wyszukaj...' />
-      </Grid.Column>
-      <Grid.Column as={Form}>
-        <Form.Input
-          label={`Ilość dni: ${show}`}
-          min={2}
-          max={21}
-          name='show'
-          onChange={this.handleChange}
-          step={1}
-          type='range'
-          value={show}
-        />
-        <Form.Button content='Szukaj' onClick={this.toggleVisibility} />
-      </Grid.Column>
-      <Grid.Column>
-      </Grid.Column>
-
-    </Grid.Row>
-
-  </Grid>
-
-
-
-    </div> 
-  );
+          <Grid.Row columns={3}>
+            <Grid.Column>
+              <Dropdown
+                placeholder='Wybierz kontynent'
+                focus
+                search
+                selection
+                options={continentsOptions}
+              >
+              </Dropdown>
+            </Grid.Column>
+           <Grid.Column as={Form}>
+              <Form.Input
+                label={`Cena: ${show} PLN`}
+                min={99}
+                max={19999}
+                name="show"
+                onChange={this.handleChange}
+                step={100}
+                type="range"
+                value={show}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Input focus icon="search" placeholder="Wyszukaj..." />
+            </Grid.Column>
+           </Grid.Row>
+            <Grid.Row columns={1}>
+              <Grid.Column>
+                <Form.Button content="Szukaj" onClick={this.toggleVisibility} />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        <TripContainer />
+      </div>
+    );
 };
 }
 export default Search;
