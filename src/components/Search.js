a0b0ee4127b0a21044b9b26bb89162b75ce3eccd
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-19 16:35:24 +0100

    Grid added to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Grid, Image } from 'semantic-ui-react';
+import TripContainer from './TripContainer';
 
 function Search() {
   return (
@@ -6,8 +8,33 @@ function Search() {
     <div className="Search">
       <h1>what R You looking for?</h1>
 
+  <Grid>
+    <Grid.Row columns={1}>
+      <Grid.Column>
+        <Image src='/images/wireframe/image.png' />
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row columns={3}>
+      <Grid.Column>
+        <Image src='/images/wireframe/image.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='/images/wireframe/image.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='/images/wireframe/image.png' />
+      </Grid.Column>
+
+    </Grid.Row>
+
+  </Grid>
+
+
+
     </div> 
   );
 };
 
 export default Search;
+
