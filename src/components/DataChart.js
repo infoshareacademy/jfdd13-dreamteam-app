82b39b7493137c9a20f1fb83f9bc11d73f4a9dea
rednaxela700 <ohleg@windowslive.com>
2019-11-20 19:45:40 +0100

    commented out second bar from barChart

--- src/components/DataChart.js
+++ src/components/DataChart.js
@@ -35,7 +35,7 @@ export default class DataBarChart extends PureComponent {
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
+                {/*<Bar dataKey="pv" fill="#8884d8" />*/}
                 <Bar dataKey="uv" fill="#82ca9d" />
             </BarChart>
             </div>
