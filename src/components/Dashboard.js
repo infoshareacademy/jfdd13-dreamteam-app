108f4c2ad40e2a94cb9d7714c07fe0a5ef8a86ca
rednaxela700 <ohleg@windowslive.com>
2019-11-22 23:22:22 +0100

    added text content to Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,29 +9,37 @@ function Dashboard() {
             <Grid padded={true}>
                 <Grid.Row>
                     <Grid.Column>
-                        <h2>Ptaki latają kluczem</h2>
+                        <h2>Planowanie podróży nigdy nie było tak proste i przyjemne</h2>
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row >
+                <Grid.Row>
                     <Grid.Column width={8}>
-                        <h2>fajny ten donat</h2>
                         <PieChartComponent/>
                     </Grid.Column>
                     <Grid.Column width={8} verticalAlign={'middle'}>
-                    <h2>a słupki śmiesznie skaczą</h2>
                         <DataBarChart/>
                     </Grid.Column>
                 </Grid.Row>
                 <Grid.Row centered={true}>
                     <Grid.Column width={8} verticalAlign={'middle'}>
-                        <h3>więcej lorem ipsum</h3>
-                        <p>chcemy więcej </p>
-                        <p>więcej lorem ipsum</p>
+                        <h3>Ustaw swój cel</h3>
+                        <div>
+                            <ul>
+                                <li>Wybierz interesujący Cię kontynent</li>
+                                <li>Oszacuj swój budżet</li>
+                                <li>Przeżyj przygodę swojego życia</li>
+                            </ul>
+                        </div>
                     </Grid.Column>
                     <Grid.Column width={8} verticalAlign={'middle'}>
-                        <h3>co mówią nam te słupeczki?</h3>
-                        <p>...a jak pozbędziemy się tych obrazków imitujących paragrafy </p>
-                        <p>i w to miejsce damy prawdziwe paragrafy, takie z krwi i kości?</p>
+                        <h3>Podróż na każdą kieszeń, zawsze.</h3>
+                        <div>
+                            <ul>
+                                <li>Wyjdź z domu i poczuj się turystą we własnym mieście</li>
+                                <li>Skorzystaj z formularza i dodaj swoją wycieczkę</li>
+                                <li>Wracaj do swoich najlepszych lokalizacji z pomocą ulubionych</li>
+                            </ul>
+                        </div>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
