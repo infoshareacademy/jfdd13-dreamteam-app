758089bfd2291a649b640474044c66270ee62a93
Rafał Wensław <rafalwenslaw@gmail>
2019-11-22 16:41:09 +0100

    Finalpage is ready

--- src/components/FinalPage.js
+++ src/components/FinalPage.js
@@ -1,61 +1,74 @@
-import React, { Component } from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
-import {Heart} from './Heart'  
+import React, { useState } from 'react';
+import { Grid, Image, Header, Container, Pagination, Modal, Button } from 'semantic-ui-react';
+const imgSrc ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s';
 
-class FinalPage extends Component {
-  
-  state = { open: false, favourites: false }
-  
-  show = (dimmer) => () => this.setState({ dimmer, open: true })
-  close = () => this.setState({ open: false })
+function TripContainer() {
+  const [open, setOpen] = useState(false)
+  const [favourites, setFavourites] = useState(false) 
+  const trips = [imgSrc, imgSrc, imgSrc]
+  const show = () => setOpen(true)
+  const close = () => setOpen(false);
+  return (
+      <div><Grid padded={true} container style={{ flex: 1, justifyContent: 'flex-start', flexDirection: 'column' }}>
+        <Grid.Row columns={1} style={{flex: 0}}>
+          <Grid.Column>
+            <Header>Tutaj znajdziesz wszystkie nasze fantastyczne wycieczki</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={3} style={{ flex: 1 }}>
+          {trips.map(trip => {
+            return <Grid.Column key={trip}>
+              <Image onClick={show} src={trip} centered={true}/>
+            </Grid.Column>
+          })}
+        </Grid.Row>
+        <Grid.Row columns={1} centered={true} style={{ minHeight: '100px' }}>
+          <Pagination
+              defaultActivePage={1}
+              firstItem={null}
+              lastItem={null}
+              pointing
+              secondary
+              totalPages={3}
+          />
+        </Grid.Row>
+      </Grid>
+      <Modal dimmer={'blurring'} open={open} onClose={close}>
+      <Modal.Header>Gdynia Główna Osobowa</Modal.Header>
+      <Modal.Content image>
+        <Image
+          wrapped
+          size='medium'
+          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s'
+        />
+        <Modal.Description>
+          <Header>Gdynia</Header>
+          <p>
+            <li>Lokalizacja:Gdynia, wygwizdowie</li>
+            <li>Liczba łóżek:1 dla 6 osób</li>
+            <li>Łazienka: brak</li>  
+            <li>Toaleta:500m od lokalizacji</li>                  
+          </p>
+          <p>Możesz polubić</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={close}>
+          Wyjdź
+        </Button>
+        <Button
+          positive
+          icon={`heart ${favourites ? '' : 'outline'}`}
+          labelPosition='right'
+          content = {favourites ? "Ulubione" : "Dodaj do ulubionych"}
+          // content="Ulubione"
+          onClick={() => {setFavourites(!favourites)}}
+          />
 
-  render() {
-    const { open, dimmer } = this.state
+      </Modal.Actions>
+    </Modal>
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <Button onClick={this.show('blurring')}>Zdjęcie miniaturka</Button>
-
-        <Modal dimmer={dimmer} open={open} onClose={this.close}>
-          <Modal.Header>Gdynia Główna Osobowa</Modal.Header>
-          <Modal.Content image>
-            <Image
-              wrapped
-              size='medium'
-              src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Ulica%C5%9Awi%C4%99toja%C5%84ska_Gdynia.jpg/1200px-Ulica%C5%9Awi%C4%99toja%C5%84ska_Gdynia.jpg'
-            />
-            <Modal.Description>
-              <Header>Gdynia</Header>
-              <p>
-                <li>Lokalizacja:Gdynia, wygwizdowie</li>
-                <li>Liczba łóżek:1 dla 6 osób</li>
-                <li>Łazienka: brak</li>  
-                <li>Toaleta:500m od lokalizacji</li>                  
-
-              </p>
-              <p>Możesz polubić</p>
-            </Modal.Description>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button color='black' onClick={this.close}>
-              Wyjdź
-            </Button>
-            <Button
-              positive
-              icon={`heart ${this.state.favourites ? '' : 'outline'}`}
-              labelPosition='right'
-              // content = {this.state.active ? "Ulubione" : "Dodaj do ulubionych"}
-              content="Ulubione"
-              onClick={() => {this.setState({favourites : !this.state.favourites})}}
-              
-              />
-    
-          </Modal.Actions>
-        </Modal>
-      </div>
-    )
-  }
-  
-
-  }
-export default FinalPage;
+export default TripContainer;
--- src/components/TripContainer.js
+++ src/components/TripContainer.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { Grid, Image, Header, Container, Pagination} from 'semantic-ui-react';
+import React, { useState } from 'react';
+
+import { Grid, Image, Header, Container, Pagination, Modal, Button } from 'semantic-ui-react';
+
+
 
 const imgSrc ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s';
 
 function TripContainer() {
+  const [open, setOpen] = useState(false)
+  const [favourites, setFavourites] = useState(false) 
+  const trips = [imgSrc, imgSrc, imgSrc]
+  const show = () => setOpen(true)
+  const close = () => setOpen(false);
   return (
-      <Grid padded={true} container style={{ flex: 1, justifyContent: 'flex-start', flexDirection: 'column' }}>
+      <div><Grid padded={true} container style={{ flex: 1, justifyContent: 'flex-start', flexDirection: 'column' }}>
         <Grid.Row columns={1} style={{flex: 0}}>
           <Grid.Column>
             <Header>Tutaj znajdziesz wszystkie nasze fantastyczne wycieczki</Header>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row columns={3} style={{ flex: 1 }}>
-          <Grid.Column>
-            <Image src={imgSrc} centered={true}/>
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={imgSrc} centered={true}/>
-          </Grid.Column>
-          <Grid.Column>
-            <Image src={imgSrc} centered={true}/>
-          </Grid.Column>
+          {trips.map(trip => {
+            return <Grid.Column key={trip}>
+              <Image onClick={show} src={trip} centered={true}/>
+            </Grid.Column>
+          })}
         </Grid.Row>
         <Grid.Row columns={1} centered={true} style={{ minHeight: '100px' }}>
           <Pagination
@@ -33,6 +37,43 @@ function TripContainer() {
           />
         </Grid.Row>
       </Grid>
+      <Modal dimmer={'blurring'} open={open} onClose={close}>
+      <Modal.Header>Gdynia Główna Osobowa</Modal.Header>
+      <Modal.Content image>
+        <Image
+          wrapped
+          size='medium'
+          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s'
+        />
+        <Modal.Description>
+          <Header>Gdynia</Header>
+          <p>
+            <li>Lokalizacja:Gdynia, wygwizdowie</li>
+            <li>Liczba łóżek:1 dla 6 osób</li>
+            <li>Łazienka: brak</li>  
+            <li>Toaleta:500m od lokalizacji</li>                  
+
+          </p>
+          <p>Możesz polubić</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color='black' onClick={close}>
+          Wyjdź
+        </Button>
+        <Button
+          positive
+          icon={`heart ${favourites ? '' : 'outline'}`}
+          labelPosition='right'
+          content = {favourites ? "Ulubione" : "Dodaj do ulubionych"}
+          // content="Ulubione"
+          onClick={() => {setFavourites(!favourites)}}
+          
+          />
+
+      </Modal.Actions>
+    </Modal>
+    </div>
   );
 };
 
