010d9cf777d722f788a483b671b0c9aaf3b7e32d
rednaxela700 <ohleg@windowslive.com>
2019-11-22 22:40:32 +0100

    changed data arr for PieChart in PieChart.js

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -4,10 +4,12 @@ import {
 } from 'recharts';
 
 const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
+    { name: 'Azja', value: 400 },
+    { name: 'Europa', value: 300 },
+    { name: 'Ameryka', value: 300 },
+    { name: 'Ameryka Południowa', value: 200 },
+    { name: 'Antarktyda', value: 200 },
+    { name: 'Australia', value: 200 }
 ];
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
