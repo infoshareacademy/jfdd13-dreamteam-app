9f17071ded02cef75d2dd7764393ee175797e1ff
Adam Szabłowski <adam.szablowski@grupawp.pl>
2019-11-23 19:04:40 +0100

    podpowiedz

--- src/components/DataChart.js
+++ src/components/DataChart.js
@@ -21,14 +21,12 @@ const data = [
 export default class DataBarChart extends PureComponent {
 
     render() {
-        return (<div style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end',height: '500px'}}>
+        return (<div>
             <BarChart
                 width={500}
                 height={300}
                 data={data}
-                margin={{
-                    top: 5, right: 30, left: 20, bottom: 5,
-                }}
+                
             >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
