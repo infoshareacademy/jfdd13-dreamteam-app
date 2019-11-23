8220a627bdc48c83b19f94366960cd50d8d3cc59
rednaxela700 <ohleg@windowslive.com>
2019-11-23 19:02:11 +0100

    changed pieChart attributes according to trainers advice

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,16 +1,16 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import {
     PieChart, Pie, Sector, Cell, Tooltip
 } from 'recharts';
 
 const data = [
-    { name: 'Azja', value: 400 },
-    { name: 'Europa', value: 300 },
-    { name: 'Ameryka', value: 300 },
-    { name: 'Afryka', value: 150 },
-    { name: 'Ameryka Południowa', value: 200 },
-    { name: 'Antarktyda', value: 200 },
-    { name: 'Australia', value: 200 }
+    {name: 'Azja', value: 400},
+    {name: 'Europa', value: 300},
+    {name: 'Ameryka', value: 300},
+    {name: 'Afryka', value: 150},
+    {name: 'Ameryka Południowa', value: 200},
+    {name: 'Antarktyda', value: 200},
+    {name: 'Australia', value: 200}
 ];
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
@@ -33,29 +33,31 @@ const renderCustomizedLabel = ({
 export default class PieChartComponent extends PureComponent {
 
     render() {
-        return (<div style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '500px'}}>
-            <PieChart width={400} height={300}>
-                <Pie
-                    data={data}
-                    cx={200}
-                    cy={200}
-                    labelLine={false}
-                    // label={renderCustomizedLabel}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    viewBox="0 100 400 200"
-                    dataKey="value"
-                    margin={{top: 0, right: 'auto', left: 'auto', bottom: 0}}
+        return (
+            <div
+                style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-end', height: '500px'}}>
+                <PieChart width={310} height={310}>
+                    <Pie
+                        data={data}
+                        cx={150}
+                        cy={150}
+                        labelLine={false}
+                        // label={renderCustomizedLabel}
+                        outerRadius={150}
+                        fill='#8884D8'
+                    // viewBox='0 0 300 300'
+                    dataKey='value'
+                    //margin={{top: 0, right: ‘auto’, left: ‘auto’, bottom: 0}}
                     style={{display: 'flex', alignItems: 'center'}}
-                    label
                 >
                     {
-                        data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+                        data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>)
                     }
                 </Pie>
                 <Tooltip/>
             </PieChart>
-            </div>
-        );
+    </div>
+    )
+
     }
 }
