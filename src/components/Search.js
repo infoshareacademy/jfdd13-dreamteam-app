b2be4ea7c3b3b0ef8cac58dbf5cbaf8ec36a2844
rednaxela700 <ohleg@windowslive.com>
2019-11-25 23:55:50 +0100

    significant changes in search - rendering dynamic output from data.js titles

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import { Grid, Input, Dropdown, Form, Container } from 'semantic-ui-react';
 import TripContainer from './TripContainer';
+import {data} from '../data'
 
 const continents = [
   {key: 'afr', value:1, text: "Afryka"},
@@ -11,48 +12,62 @@ const continents = [
   {key: 'azj', value:5, text: "Azja"},
   {key: 'eur', value:6, text: "Europa"}
 ];
-const places = [
-  {value:'Amsterdam'},
-  {value:'Bangkok'},
-  {value:'Berlin'},
-  {value:'Innsbruck'},
-  {value:'Johannesburg'},
-  {value:'Kraków'},
-  {value:'Lizbona'},
-  {value:'Londyn'},
-  {value:'Los Angeles'},
-  {value:'Madryt'},
-  {value:'Mexico City'},
-  {value:'Monachium'},
-  {value:'Moskwa'},
-  {value:'Neapol'},
-  {value:'Nowy Jork'},
-  {value:'Rzym'},
-  {value:'Sydney'},
-  {value:'Tokio'},
-  {value:'Trójmiasto'},
-  {value:'Vancouver'},
-  {value:'Wiedeń'},
-];
+
+const SeachConst = props => {
+  const {searchQuery,
+  onChange,
+  search
+  } = props;
+
+  return (
+      <Fragment>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={onChange}
+        />
+      </Fragment>
+  )
+};
+
 
 class Search extends Component {
-  state = {show: 999}
+  state = {
+    show: 999,
+    searchQuery: '',
+    results: []
+  };
+  handleSearchQuery (e) {
+    this.setState({searchQuery: e.target.value});
+  }
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value });
   toggleVisibility = () =>
     this.setState((prevState) => ({ visible: !prevState.visible }));
 
+  handleSearchResult = (props) => {
+    console.log('dupa');
+    console.log(props);
+  }
+
   render() {
-    const {show} = this.state;
+    const {show, searchQuery} = this.state;
 
     return (
       <div className="search">
+        {JSON.stringify(data.filter(query => query.title.toLowerCase().includes(searchQuery)))}
         <Grid padded={true}>
           <Grid.Row columns={1} centered={true}>
             <Grid.Column width={12}>
-              <Input list='places' placeholder='Podaj destynację...' fluid  />
+              <Input
+                  onChange={event => this.handleSearchQuery(event)}
+                  list='places'
+                  placeholder='Podaj destynację...'
+                  fluid
+                  onKeyPress={this.handleSearchResult}
+              />
                 <datalist id='places'>
-                  {places.map(v => <option> {v.value}</option>)}
+                  {data.map(v => <option> {v.city }</option>)}
                 </datalist>
           </Grid.Column>
           </Grid.Row>
