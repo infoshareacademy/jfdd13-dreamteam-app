678e6344eb0365c65cd676c07f889ed8e6922df9
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-27 21:19:59 +0100

    UserPanel component  - with routing setups

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Dashboard from "./components/Dashboard";
 import TripContainer from "./components/TripContainer";
 import Form from "./components/Form";
 import Search from "./components/Search";
-import Favourites from "./components/Favourites";
+import Favourites from "./components/UserPanel";
 import Appbar from "./components/Appbar";
 // import FinalPage from "./components/FinalPage";
 import 'semantic-ui-css/semantic.min.css'
+import UserPanel from './components/UserPanel';
 
 function App() {
     return (
@@ -23,8 +24,8 @@ function App() {
                         <Route exact strict path="/main" component={Dashboard}/>
                         <Route exact strict path="/trip" component={TripContainer}/>
                         <Route exact strict path="/search" component={Search}/>
-                        <Route exact strict path="/fav" component={Favourites}/>
-                        <Route exact strict path="/form" component={Form}/>
+                        <Route exact strict path="/panel" component={UserPanel}/>
+                        <Route exact strict path="/form/" component={Form}/>
                         <Route component={() => <h1>404 - sadface</h1>}/>
                     </Switch>
                 </main>
