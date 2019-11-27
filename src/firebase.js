0afd199bb583a90c576e95b1d141c76da42bbed7
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-27 10:59:11 +0100

    initial - firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID
+} = process.env;
+
+const firebaseConfig = {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
