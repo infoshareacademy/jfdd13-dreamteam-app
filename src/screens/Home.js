713fd0c3f0265947386d1ee5950ac183e28290e3
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-28 15:40:31 +0100

    switched from class to function component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import {Button, Container,Menu, Segment } from "semantic-ui-react";
+import {NavLink} from 'react-router-dom';
+import {Button, Container, Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
 
-class Home extends Component {
+class Home1 extends Component {
   state = {};
 
   render() {
@@ -53,6 +54,64 @@ class Home extends Component {
           </Segment>
     );
   }
+}
+
+function Home () {
+
+  const navStyle = {
+    width: '100vw',
+    minHeight: '100px',
+    position: 'right',
+    border: 0,
+    borderRadius: 0
+  };
+  return (
+  <Sidebar.Pushable as={Segment} style={navStyle}>
+    <Sidebar 
+      as={Menu}
+      animation='overlay'
+      icon='labeled'
+      inverted
+      direction='top'
+      visible={true}
+      width='thin'
+      
+    >
+      <NavLink to="/" exact>
+      <Menu.Item
+           onClick={() => {
+            fetch('https://dreamteam-app.firebaseio.com/test.json', {
+              method: 'POST',
+              body: JSON.stringify({
+                test: 'just onClick test', 
+                date: new Date().toLocaleString()
+              }).toLowerCase() 
+            }); console.log('you have sent a test')
+          }}
+      >
+        <Icon name='text cursor' />
+        Test database
+      </Menu.Item>
+      </NavLink>
+
+      <NavLink to="/login" exact>
+      <Menu.Item>
+        <Icon name='user secret' />
+        Zaloguj
+      </Menu.Item>
+      </NavLink>
+      
+      <NavLink to="/register" exact>
+      <Menu.Item>
+        <Icon name='registered' />
+        Zarejestruj
+      </Menu.Item>
+      </NavLink>
+     
+    </Sidebar>
+  </Sidebar.Pushable>
+
+  )
 }
 
 export default Home;
