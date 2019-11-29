ff2baca4ade1a72ca5330db68c770cb91915ca62
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-29 02:06:44 +0100

    changed to function component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,60 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import {NavLink} from 'react-router-dom';
-import {Button, Container, Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
-
-class Home1 extends Component {
-  state = {};
-
-  render() {
-    const { fixed } = this.state;
-
-    return (
-          <Segment
-            inverted
-            textAlign="center"
-            style={{ minHeight: 10, padding: "1em 0em" }}
-            vertical
-          >
-            <Menu
-              fixed={fixed ? "top" : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size="large"
-            >
-              <Container>
-                <Menu.Item position="right">
-                  <Button as="a"  inverted={!fixed}
-                  onClick={() => {
-                    fetch('https://dreamteam-app.firebaseio.com/test.json', {
-                      method: 'POST',
-                      body: JSON.stringify({
-                        test: 'just onClick test', 
-                        date: new Date().toLocaleString()
-                      }).toLowerCase() 
-                    }); console.log('you have sent a test')
-                  }}
-                  > 
-                    Test database
-                  </Button>
-                  <Button as="a" inverted={!fixed}>
-                    Zaloguj
-                  </Button>
-                  <Button
-                    as="a"
-                    inverted={!fixed}
-                    primary={fixed}
-                    style={{ marginLeft: "0.5em" }}
-                  >
-                    Zarejestruj
-                  </Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
-          </Segment>
-    );
-  }
-}
+import {Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
 
 function Home () {
 
@@ -85,39 +31,35 @@ function Home () {
                 test: 'just onClick test', 
                 date: new Date().toLocaleString()
               }).toLowerCase() 
-            }); console.log('you have sent a test')
-          }}
+            });
+          }}          
       >
-        <Icon name='text cursor' />
+        <Icon name='upload' />
         Test database
       </Menu.Item>
       </NavLink>
       <NavLink to="#" exact>
       <Menu.Item
            onClick={() => {
-            fetch('https://dreamteam-app.firebaseio.com/test.json', {
-              method: 'GET',
-              body: JSON.stringify({
-                test: 'just onClick test', 
-                date: new Date().toLocaleString()
-              }).toLowerCase() 
-            }); console.log('you have sent a test')
+            fetch('https://dreamteam-app.firebaseio.com/test.json'); 
+            console.log('getting news')
           }}
       >
-        <Icon name='text cursor' />
-        Test database
+        <Icon name='download' />
+        GET from database
       </Menu.Item>
       </NavLink>
+
       <NavLink to="/login" exact>
       <Menu.Item>
-        <Icon name='user secret' />
+        <Icon name='sign in' />
         Zaloguj
       </Menu.Item>
       </NavLink>
 
       <NavLink to="/register" exact>
       <Menu.Item>
-        <Icon name='registered' />
+        <Icon name='signup' />
         Zarejestruj
       </Menu.Item>
       </NavLink>
