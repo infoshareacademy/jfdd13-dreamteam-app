b5e9a419398e2a17d6ffdc10fb4800a6e23c0fe4
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-29 16:55:46 +0100

    testing the Test func

--- src/screens/Test.js
+++ src/screens/Test.js
@@ -1,26 +1,37 @@
 import React from 'react';
 
-export const testLocalStorageOnClick = () => {
+const testData = JSON.parse(localStorage.getItem('form'));
 
+export const testLocalStorageOnClick = () => {
   return (<p> 
-           fetch('https://dreamteam-app.firebaseio.com/test.json'); 
-     
+    fetch('https://dreamteam-app.firebaseio.com/test.json'); 
     {JSON.parse(localStorage.getItem('click'))} 
     </p>
   )
 }
 
-const Test = props => {
-  // const [test, setTest] = useState("")
+const Test = () => {
  
     return (
     <div>
-      {/* <p>{localStorage.getItem('click')}</p> */}
-        <p>{localStorage.getItem('form')}</p>
-        {/* <p>{JSON.parse(localStorage.getItem('form'))}</p> */}
-        
+      <ul>
+        {testData.map(item => {
+      
+      <li>{item.price}</li>
+      <li>{item.description}</li>}
+          
+          )}
+         </ul>
+    </div>
+    )
+     {/* {testData.map(obj => {
+                const [city, continent, data, description, email, price, terms, title] = obj;
 
-    </div>)
+        return [obj.city, obj.continent, obj.data, obj.description, obj.email, obj.price, obj.terms, obj.title];
+     }).catch(err => console.log(err))
+    } */}
+     
+    
 }
 
 export default Test;
