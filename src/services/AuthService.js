26e1139f5a8596f465c6b4e845765f7876e6023c
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-02 15:46:40 +0100

    improved function login

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,20 +1,15 @@
 import firebase from "../firebase";
 
-export const login = (email, password) => {
-  return firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(value => {
-      console.log("Zalogowano!");
-      console.log(value);
-    })
-    .then(value => {
-      const user = firebase.auth().currentUser;
-    })
-    .catch(() => {
-      console.log("Spróbuj jeszcze raz!");
-    });
-};
+export async function login (email, password) {
+  try{
+  const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password)
+  const id = userCredential.user.uid
+  console.log("Zalogowano!");
+ 
+  } catch (error) {
+    console.log("Spróbuj jeszcze raz!");
+  }
+} 
 
 export const signout = (user) => {
   return firebase
@@ -25,7 +20,7 @@ export const signout = (user) => {
       });
 };
 
-export async function register (email, password, name) => {
+export async function register (email, password, name) {
   try{
     const userCredential = await firebase.auth.createUserWithEmailAndPassword(email, password)
 
