8d10c94335bf75349f6fcf4ca6e5e3d60fb823fa
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-02 15:56:35 +0100

    sendTest added

--- src/services/TestService.js
+++ src/services/TestService.js
@@ -1,17 +1,14 @@
 import firebase from "../firebase";
 
-export const sendTrip = trip => {
-  const userId = firebase.auth().currentUser.uid;
-  firebase
-    .database()
-    .ref("/trip")
-    .push({
-      userId,
-      test,
-      createdAt: new Date().toISOString()
-    });
-};
-
+export async function sendTest (test) {  
+  try{
+    await firebase.database().ref("/test").set({test})
+    console.log("test wysłany!");  
+  } catch (error) {
+    console.log("test nie został wysłany");
+  }
+ };
+  
 const prepareTrips = data => {
   return Object.entries(data).map(arr => {
     const [id, value] = arr;
@@ -30,33 +27,4 @@ export const watchTrips = onSuccess => {
       const trips = dataSnapshot.val();
       onSuccess(prepareTrips(trips));
     });
-};
-
-export const stopTrips = () => {
-  firebase
-    .database()
-    .ref("/trip")
-    .off();
-};
-
-
-//////userService?//////
-
-const prepareUsers = data => Object.values(data);
-
-export const watchUsers = onSuccess => {
-  return firebase
-    .database()
-    .ref("/users")
-    .on("value", dataSnapshot => {
-      const users = dataSnapshot.val();
-      onSuccess(prepareUsers(users));
-    });
-};
-
-export const stopUsers = () => {
-  firebase
-    .database()
-    .ref("/users")
-    .off();
 };
