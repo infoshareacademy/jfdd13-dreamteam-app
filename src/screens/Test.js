e0b8031154b8519839f965ed7923148acb3ec3fb
rednaxela700 <ohleg@windowslive.com>
2019-12-03 16:28:23 +0100

    fixed Test.js

--- src/screens/Test.js
+++ src/screens/Test.js
@@ -1,40 +1,47 @@
 import React from 'react';
+import {Grid, Header, Image} from 'semantic-ui-react';
 
-const dataCheck = JSON.parse(localStorage.getItem('form'));
-const testDataImport = (dataCheck) || []
-
-
-export const testLocalStorageOnClick = () => {
-  return (<p> 
-    fetch('https://dreamteam-app.firebaseio.com/test.json'); 
-    {JSON.parse(localStorage.getItem('click'))} 
-    </p>
-  )
-}
+const localSdata = JSON.parse(localStorage.getItem('form'));
+const dataFromLocalStorage = (localSdata)? localSdata : [];
 
 const Test = () => {
- 
-    return (
-    <div>
-      <ul>
-          {
-              testDataImport.map((item, index) => (
 
-                  <li key={index}>{item.price}</li>
-                  // <li>{item.description}</li>
-              ))
-              }
-         </ul>
-    </div>
-    )
-     {/* {testData.map(obj => {
+    return (
+        <div>
+            <Header>tu renderujemy dane z wypełnionego Formularza (zapis do localStorage) </Header>
+            {[dataFromLocalStorage].map(trip => (
+                <Grid.Column style={{padding: "0 2rem"}}>
+                    <Image
+                        className="TripImage"
+                        // onClick={() => show(trip.id)}
+                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s'
+                        label={{
+                            ribbon: true,
+                            color: "blue",
+                            content: `${trip.city}`
+                        }}
+                        centered={true}
+                    />
+                    <ul style={{'listStyleType': 'none'}}>
+                        <li>{trip.title}</li>
+                        <li>miasto: {trip.city}</li>
+                        <li>kontynent: {trip.continent}</li>
+                        <li>data: {trip.date}</li>
+                        <li>cena: {trip.price} PLN</li>
+                        <li>opis: {trip.description}</li>
+                        <li>kontakt: {trip.email}</li>
+                    </ul>
+                </Grid.Column>))}
+        </div>
+    );
+}
+
+{/* {testData.map(obj => {
                 const [city, continent, data, description, email, price, terms, title] = obj;
 
         return [obj.city, obj.continent, obj.data, obj.description, obj.email, obj.price, obj.terms, obj.title];
      }).catch(err => console.log(err))
-    } */}
-     
-    
+    } */
 }
 
 export default Test;
