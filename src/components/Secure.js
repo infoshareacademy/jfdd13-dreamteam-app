2f5c214c5b30406f6c646b4ed301a3007608ab80
michal-kusnierz <kusnierz.michal@gmail.com>
2019-12-03 16:35:07 +0100

    component modyfied from class  to function with routing: Login or Register

--- src/components/Secure.js
+++ src/components/Secure.js
@@ -1,27 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import firebase from 'firebase';
-import Login from '../screens/Login';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import firebase from "firebase";
+import Login from "../screens/Login";
+import Register from "../screens/Register";
 
-class Secure extends React.Component {
-  state = {
-    user: null
+const Secure = props => {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
+
+  if (isLoggedIn == null) {
+    return <h1>Poczekaj, sprawdzam!</h1>;
   }
 
-  unsubscribe = null
-  
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => this.setState({ user }));
+  if (isLoggedIn === false) {
+    return (
+      <div>
+        <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login}>
+            <Login />
+          </Route>
+          <Route exact path="/register" component={Register}>
+            <Register />
+          </Route>  
+          <Redirect to="/login"/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 
-  // componentWillUnmount() {
-  //   if (unsubscribe) {
-  //     unsubscribe()
-  //   }
-  // }
-
-  render() {
-    return this.state.user === null ? <Login/> : this.props.children
+  if (isLoggedIn === true) {
+    return props.children;
   }
-}
+};
 
 export default Secure;
