f572abf24a3faf80735c5903c99382fb68941d8f
rednaxela700 <ohleg@windowslive.com>
2019-12-04 18:16:57 +0100

    deleted h1 from UserPanel.js

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -4,8 +4,7 @@ import Home from '../screens/Home';
 function UserPanel() {
   return (
     <div>
-      <h1 style={{backgroundColor:'yellow'}}>man at work... (with firebase)</h1>
-      <Home />
+        <Home />
     </div> 
   )
 }; 
