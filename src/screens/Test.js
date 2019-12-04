b75b65b5bb82538563f2a5a1cdb5a0aa3c58092f
rednaxela700 <ohleg@windowslive.com>
2019-12-04 15:03:35 +0100

    deleted unneccessarry content

--- src/screens/Test.js
+++ src/screens/Test.js
@@ -7,14 +7,6 @@ const dataFromLocalStorage = (localSdata) ? localSdata : console.log('dupa');
 
 const Test = () => {
 
-
-    // console.log(
-    //     data.filter(item => {
-    //         dataFromLocalStorage.find(item.id)
-    //
-    //
-    //     }))
-    // // console.log(data.filter(o => !dataFromLocalStorage.find(o2 => o.id === o2.id)))
     return (
         <div>
             {
@@ -45,43 +37,6 @@ const Test = () => {
                         </Grid.Column>))
             }
         </div>
-            )
+    )
 }
-{/*            console.log(data.filter(item => item.id === id))}*/}
-{/*            <Header>tu renderujemy dane z wypełnionego Formularza (zapis do localStorage) </Header>*/}
-{/*            {[dataFromLocalStorage].map(trip => (*/}
-{/*                <Grid.Column style={{padding: "0 2rem"}}>*/}
-{/*                    <Image*/}
-{/*                        className="TripImage"*/}
-{/*                        // onClick={() => show(trip.id)}*/}
-{/*                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s'*/}
-{/*                        label={{*/}
-{/*                            ribbon: true,*/}
-{/*                            color: "blue",*/}
-{/*                            content: `${trip.city}`*/}
-{/*                        }}*/}
-{/*                        centered={true}*/}
-{/*                    />*/}
-{/*                    <ul style={{'listStyleType': 'none'}}>*/}
-{/*                        <li>{trip.title}</li>*/}
-{/*                        <li>miasto: {trip.city}</li>*/}
-{/*                        <li>kontynent: {trip.continent}</li>*/}
-{/*                        <li>data: {trip.date}</li>*/}
-{/*                        <li>cena: {trip.price} PLN</li>*/}
-{/*                        <li>opis: {trip.description}</li>*/}
-{/*                        <li>kontakt: {trip.email}</li>*/}
-{/*                    </ul>*/}
-{/*                </Grid.Column>))}*/}
-{/*        </div>*/}
-{/*    );*/}
-{/*};*/}
-
-{/* {testData.map(obj => {
-                const [city, continent, data, description, email, price, terms, title] = obj;
-
-        return [obj.city, obj.continent, obj.data, obj.description, obj.email, obj.price, obj.terms, obj.title];
-     }).catch(err => console.log(err))
-    } */
-}
-
 export default Test;
