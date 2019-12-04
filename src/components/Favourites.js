e0e03f92c2a982cec47043dbd9b7b6fb4c8b2dee
rednaxela700 <ohleg@windowslive.com>
2019-12-04 18:31:28 +0100

    rendering img from DB

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Header, Grid, Image } from "semantic-ui-react";
 import { fetchTrips } from "../services/TripService";
-import firebase from "../firebase";
 
 const Favourites = (props) => {
   const [trips, setTrips] = useState([]);
@@ -13,14 +12,13 @@ const Favourites = (props) => {
   },[])
 
   return (<div trips={trips}>
-        <Header>dane z firebase dla usera {firebase.auth().currentUser.uid} </Header>
       {console.log(trips)}
       {trips.map(trip => (
         <Grid.Column  style={{ padding: "0 2rem" }}>
           <Image
             className="TripImage"
             // onClick={() => show(trip.id)}
-            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s'
+            src={trip.tripImageUrl || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s'}
             label={{
               ribbon: true,
               color: "blue",
