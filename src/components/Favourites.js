0a31a1bf69fae751289ac33e0646d3f8ec6f9c52
rednaxela700 <ohleg@windowslive.com>
2019-12-06 16:14:35 +0100

    changed output in Favourites to Search.js grid, functionality commented out and not working

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Image } from "semantic-ui-react";
+import {Grid, Icon, Image} from "semantic-ui-react";
 import { fetchTrips } from "../services/TripService";
 
 const Favourites = () => {
@@ -11,31 +11,40 @@ const Favourites = () => {
     })
   },[]);
 
-  return (<div>
-      {trips.map(trip => (
-        <Grid.Column key={trip.city} style={{ padding: "0 2rem" }}>
-          <Image
-            className="TripImage"
-            src={trip.tripImageUrl || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s'}
-            label={{
-              ribbon: true,
-              color: "blue",
-              content: `${trip.city}`
-            }}
-            centered={true}
-          />
-          <ul style={{'listStyleType':'none'}}>
-            <li>{trip.title}</li>
-            <li>miasto: {trip.city}</li>
-            <li>kontynent: {trip.continent}</li>
-            <li>data: {trip.date}</li>
-            <li>cena: {trip.price} PLN</li>
-            <li>opis: {trip.description}</li>
-            <li>kontakt: {trip.email}</li>
-          </ul>
-        </Grid.Column>))}
+  return (trips.map(trip => (
+      <div key={trip.id}>
+        <Grid.Column style={{padding: '0 2rem'}} onClick={() => {
+          this.setState({
+            selectedTrip: trip
+          })
+        }}>
+          <div style={{position: 'relative'}}>
+            <Image
+                className="TripImage"
+                // onClick={() => rangeValue(trip.id)}
+                src={trip.tripImageUrl || }
+                label={{
+                  ribbon: true,
+                  color: "blue",
+                  content: `${trip.city}`
+                }}
+                centered={true}
+            >
+            </Image>
+            <Icon
+                className={'iconFavourites'}
+                size={'large'}
+                inverted
+                // name={this.state.favourites.includes(trip.id) ? 'heart' : 'heart outline'}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  this.handleFavIcon(trip.id)
+                }}/>
+          </div>
+          <p>{trip.title}</p>
+        </Grid.Column>
       </div>
-    )
+    )))
 } 
 
 export default Favourites;
