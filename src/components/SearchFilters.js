503641b113fad565d63cbcb2e1e8b5e6a1747e7b
rednaxela700 <ohleg@windowslive.com>
2019-12-16 14:02:22 +0100

    exported continents array to seperate component

--- src/components/SearchFilters.js
+++ src/components/SearchFilters.js
@@ -1,14 +1,5 @@
 import React from 'react'
 
-export const Continents = [
-    { key: 'afr', value: 1, text: "Afryka" },
-    { key: 'apd', value: 2, text: "Ameryka Południowa" },
-    { key: 'apn', value: 3, text: "Ameryka Północna" },
-    { key: 'aus', value: 4, text: "Australia i Oceania" },
-    { key: 'azj', value: 5, text: "Azja" },
-    { key: 'eur', value: 6, text: "Europa" }
-];
-
 export const FilteredResults = () => {
     const { searchQuery, selectedContinent, rangeValue } = this.state;
     const continent = Continents.find(continent => {
