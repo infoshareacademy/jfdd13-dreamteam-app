49ca5f017c88f69a10525851a7d62f5be05456fd
rednaxela700 <ohleg@windowslive.com>
2020-09-13 02:25:18 +0200

    added Hooked function in Search.js, rewriting Search Class there. Left todos

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,51 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment, useState, useEffect} from 'react';
 import {ShowLoader} from "./Loader";
-import { Grid, Input, Dropdown, Form, Image, Icon, Modal, Header, Button } from 'semantic-ui-react';
-import { data } from '../data'
-import { fetchTrips, fetchFromFavorites, stopFetching, toggleFavorite } from "../services/TripService";
+import {Grid, Input, Dropdown, Form, Image, Icon, Modal, Header, Button} from 'semantic-ui-react';
+import {data} from '../data'
+import {fetchTrips, fetchFromFavorites, stopFetching, toggleFavorite} from "../services/TripService";
 import {Continents} from "./Continents";
 
 const initialRange = 1999;
 const defaultImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s';
 
+const Hooked = () => {
+    const [dropdownValue, setDropdownValue] = useState('');
+    const [rangeValue, setRangeValue] = useState(initialRange);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [drop, setDrop] = '';
+    const [results, setResults] = [];
+    const [searchTargetValue, setSearchTargetValue] = '';
+    const [selectedContinent, setSelectedContinent] = '';
+    const [selectedTrip, setSelectedTrip] = null;
+    const [favourites, setFavourites] = [];
+    const [fetched, setFetched] = false;
+
+    useEffect(() => {
+        const f = async ()=> {
+            const results = await fetchTrips()
+            setResults(results)
+            await fetchFromFavorites(favourites => {
+                setFavourites(favourites)
+                setFetched(true)
+            })
+        }
+        f()
+        return () => stopFetching()
+    })
+
+    const handleFavIcon = async (tripId)=> {
+        await toggleFavorite(tripId)
+    }
+    //todo: handlers here, then the getter
+    //todo: after that finish queryOutput
+
+    const queryOutput = () => !fetched ? ShowLoader() : ( <div>
+
+        </div>
+
+    )
+
+}
 
 class Search extends Component {
     state = {
@@ -22,7 +60,7 @@ class Search extends Component {
         favourites: [],
         fetched: false
     };
-    
+
 
     async componentDidMount() {
         const results = await fetchTrips()
@@ -48,49 +86,49 @@ class Search extends Component {
     queryOutput() {
         return (
             (!this.state.fetched) ?
-                ShowLoader():
+                ShowLoader() :
                 (this.FilteredResults.length === 0) ?
                     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}}>
                         <h2>Nie ma takiej wycieczki, ale możesz ją dodać!</h2>
-                    </div>:
-                this.FilteredResults.map(trip => (
-                    <div key={trip.id} className={'tripContainer'}>
-                        <Grid.Column style={{ padding: '0 2rem' }} onClick={() => {
-                            this.setState({
-                                selectedTrip: trip
-                            })
-                        }}>
-                            <div style={{ position: 'relative' }}>
-                                <Image
-                                    className="TripImage"
-                                    // onClick={() => rangeValue(trip.id)}
-                                    src={trip.tripImageUrl || defaultImg}
-                                    label={{
-                                        ribbon: true,
-                                        color: "blue",
-                                        content: `${trip.city}`
-                                    }}
-                                    centered={true}
-                                    style={{ cursor: 'pointer' }}
-                                >
-                                </Image>
-                                <Icon
-                                    className={'iconFavourites'}
-                                    size={'large'}
-                                    inverted
-                                    name={this.state.favourites[trip.id] !== undefined ? 'heart' : 'heart outline'}
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                       this.handleFavIcon(trip.id)
-                                    }} />
-                            </div>
-                            <p>{trip.title}</p>
-                        </Grid.Column>
-                    </div>
-                ))
+                    </div> :
+                    this.FilteredResults.map(trip => (
+                        <div key={trip.id} className={'tripContainer'}>
+                            <Grid.Column style={{padding: '0 2rem'}} onClick={() => {
+                                this.setState({
+                                    selectedTrip: trip
+                                })
+                            }}>
+                                <div style={{position: 'relative'}}>
+                                    <Image
+                                        className="TripImage"
+                                        // onClick={() => rangeValue(trip.id)}
+                                        src={trip.tripImageUrl || defaultImg}
+                                        label={{
+                                            ribbon: true,
+                                            color: "blue",
+                                            content: `${trip.city}`
+                                        }}
+                                        centered={true}
+                                        style={{cursor: 'pointer'}}
+                                    >
+                                    </Image>
+                                    <Icon
+                                        className={'iconFavourites'}
+                                        size={'large'}
+                                        inverted
+                                        name={this.state.favourites[trip.id] !== undefined ? 'heart' : 'heart outline'}
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            this.handleFavIcon(trip.id)
+                                        }}/>
+                                </div>
+                                <p>{trip.title}</p>
+                            </Grid.Column>
+                        </div>
+                    ))
 
 
-    )
+        )
     }
 
     handleRange = (e) => {
@@ -111,8 +149,8 @@ class Search extends Component {
         })
     };
 
-    get FilteredResults()  {
-        const { searchQuery, selectedContinent, rangeValue } = this.state;
+    get FilteredResults() {
+        const {searchQuery, selectedContinent, rangeValue} = this.state;
         const continent = Continents.find(continent => {
             return continent.value === selectedContinent
         });
@@ -130,7 +168,7 @@ class Search extends Component {
     }
 
     render() {
-        const { selectedTrip } = this.state
+        const {selectedTrip} = this.state
 
         return (
             <div className="search">
@@ -160,36 +198,36 @@ class Search extends Component {
                             />
                         </Grid.Column>
                         <Grid.Column as={Form} widescreen={6} largeScreen={6} mobile={12} textAlign={"right"}
-                            style={{ display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}>
+                                     style={{display: 'inline-flex', alignItems: 'center', justifyContent: 'center'}}>
                             <span style={{
                                 display: 'inline-flex',
                                 padding: '0 8px',
                                 height: '100%'
                             }}>Maksymalna cena za dobę: {this.state.rangeValue || '0'}zł</span>
                             <input type={'range'}
-                                min={0}
-                                max={2000}
-                                step={100}
-                                onChange={this.handleRange}
-                                name={'show'}
-                                value={this.state.rangeValue}
-                                style={{ minHeight: '40px' }}
+                                   min={0}
+                                   max={2000}
+                                   step={100}
+                                   onChange={this.handleRange}
+                                   name={'show'}
+                                   value={this.state.rangeValue}
+                                   style={{minHeight: '40px'}}
                             />
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
                 <Grid container
-                    style={
-                        {
-                            display: 'flex',
-                            justifyContent: 'flex-start',
-                            flexDirection: 'column',
-                            height: '100%',
-                            margin: 'auto !important'
-                        }
-                    }>
+                      style={
+                          {
+                              display: 'flex',
+                              justifyContent: 'flex-start',
+                              flexDirection: 'column',
+                              height: '100%',
+                              margin: 'auto !important'
+                          }
+                      }>
                     <Grid.Row
-                        columns={3} mobile={1} style={{ display: 'flex', height: '100%' }}
+                        columns={3} mobile={1} style={{display: 'flex', height: '100%'}}
                     >
                         {this.queryOutput()}
                     </Grid.Row>
@@ -213,7 +251,7 @@ class Search extends Component {
                             />
                             <Modal.Description>
                                 <Header>{selectedTrip.city}</Header>
-                                <ul style={{ padding: "0 0 0 1.5rem" }}>
+                                <ul style={{padding: "0 0 0 1.5rem"}}>
                                     <li>{selectedTrip.continent}</li>
                                     <li>Cena za dobę za osobę: {selectedTrip.price} PLN</li>
                                     <li>Data wyjazdu: {selectedTrip.date}</li>
@@ -223,11 +261,11 @@ class Search extends Component {
                         </Modal.Content>
                         <Modal.Actions>
                             <Button color="black"
-                                onClick={() => {
-                                    this.setState({
-                                        selectedTrip: null
-                                    })
-                                }}
+                                    onClick={() => {
+                                        this.setState({
+                                            selectedTrip: null
+                                        })
+                                    }}
                             >
                                 Wyjdź
                             </Button>
