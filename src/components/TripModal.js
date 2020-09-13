78ed698faba62ebc4662889548ed59b75fdb3182
rednaxela700 <ohleg@windowslive.com>
2020-09-13 14:30:42 +0200

    src/components/TripModal.js: added destructuring and if statement

--- src/components/TripModal.js
+++ src/components/TripModal.js
@@ -1,46 +1,52 @@
 import React, {Fragment} from 'react'
 import {Button, Header, Image, Modal} from "semantic-ui-react";
+
 const defaultImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s';
 
-const TripModal = ({ selectedTrip, setSelectedTrip}) => (
-    <Modal
-        dimmer={'blurring'}
-        open={selectedTrip != null}
-        onClose={() => setSelectedTrip(null)}
-    >
-        {selectedTrip != null && <Fragment>
-            <Modal.Header>{selectedTrip.title}</Modal.Header>
-            <Modal.Content image>
-                <Image
-                    wrapped
-                    size={'large'}
-                    src={selectedTrip.tripImageUrl || defaultImg}
-                />
-                <Modal.Description>
-                    {/*todo destructure following values*/}
-                    <Header>{selectedTrip.city}</Header>
-                    <ul>
-                        <li>{selectedTrip.continent}</li>
-                        <li>Cena za dobę za osobę: {selectedTrip.price} PLN</li>
-                        <li>Data wyjazdu: {selectedTrip.date}</li>
-                        <li>Opis: {selectedTrip.description}</li>
-                    </ul>
-                </Modal.Description>
-            </Modal.Content>
-            <Modal.Actions>
-                <Button
-                    color={'black'}
-                    onClick={() => setSelectedTrip(null)}
-                >
-                    Wyjdź
-                </Button>
-                <Button
-                    positive
-                    labelPosition="right"
-                />
-            </Modal.Actions>
-        </Fragment>}
-    </Modal>
-)
+const TripModal = ({selectedTrip, setSelectedTrip}) => {
+    if (selectedTrip != null) {
+        const {title, city, continent, date, price, description, tripImageUrl} = selectedTrip
+        return (
+            <Modal
+                dimmer={'blurring'}
+                open={selectedTrip !== null}
+                onClose={() => setSelectedTrip(null)}
+            >
+                {<Fragment>
+                    <Modal.Header>{title}</Modal.Header>
+                    <Modal.Content image>
+                        <Image
+                            wrapped
+                            size={'large'}
+                            src={tripImageUrl || defaultImg}
+                        />
+                        <Modal.Description>
+                            <Header>{city}</Header>
+                            <ul>
+                                <li>{continent}</li>
+                                <li>Cena za dobę za osobę: {price} PLN</li>
+                                <li>Data wyjazdu: {date}</li>
+                                <li>Opis: {description}</li>
+                            </ul>
+                        </Modal.Description>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button
+                            color={'black'}
+                            onClick={() => setSelectedTrip(null)}
+                        >
+                            Wyjdź
+                        </Button>
+                        <Button
+                            positive
+                            labelPosition="right"
+                        />
+                    </Modal.Actions>
+                </Fragment>}
+            </Modal>
+        );
+    }
+    return null
+}
 
 export default TripModal
