6a98a6d5c706fc2b9c2e9ba5d479a1c7b004b3da
rednaxela700 <ohleg@windowslive.com>
2020-09-14 19:35:57 +0200

    src/components/Favourites.js: successfully converted class component to functional -  added useEffect running on each heart icon click -  added state with all trips (data)

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -19,44 +19,70 @@ import TripModal from "./TripModal";
 import {Continents} from "./Continents";
 
 const defaultImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTDgEOsiQyCYSqiBVVAWAxMkKz8jiz80Qu0U8MuaiGJryGMTVR&s';
-const Favourites2 = () => {
+const Favourites = () => {
+    const [data, setData] = useState([])
     const [selectedTrip, setSelectedTrip] = useState(null);
-    const [favourites, setFavourites] = useState([])
+    const [favouritesList, setFavouritesList] = useState([])
+    const [favourites, setFavourites] = useState([]);
     const [fetched, setFetched] = useState(false);
+    const [favouriteTrip, setFavouriteTrip] = useState(false);
 
     useEffect(() => {
         if (!fetched) {
             const f = async () => {
                 const allTrips = await fetchTrips()
                 await fetchFromFavorites(favourites => {
-                    const favouritesList = allTrips.filter((trip) => favourites[trip.id] !== undefined)
-                    setFavourites(favouritesList)
+                    setData(allTrips)
+                    const filteredTrips = allTrips.filter((trip) => favourites[trip.id] !== undefined)
+                    setFavouritesList(filteredTrips)
+                    setFavourites(favourites)
                     setFetched(true)
                     stopFetching()
                 })
             }
             f()
         }
-        return () => setFetched(false)
+        return () => setFetched(true)
     }, [])
+
+    useEffect(() => {
+        async function f(clickedTripId) {
+            await handleFavIcon(clickedTripId)
+            setFavouriteTrip(false)
+            await fetchFromFavorites(favourites => {
+                const filteredTrips = data.filter((trip) => favourites[trip.id] !== undefined)
+                setFavouritesList(filteredTrips)
+                stopFetching()
+            })
+        }
+
+        if (favouriteTrip) {
+            f(favouriteTrip)
+        }
+        return ()=>setFavouriteTrip(false)
+    }, [favouriteTrip])
 
     if (!fetched) {
         return null;
     }
 
+    const handleFavIcon = async (tripId) => {
+        await toggleFavorite(tripId)
+    }
 
     const queryOutput = () => {
         if (!fetched) {
             return ShowLoader()
-        } else if (favourites.length === 0) {
+        } else if (favouritesList.length === 0) {
             return (
                 <NoQueryResult/>
             )
         }
-        return favourites.map(trip => (<FilteredQueryResults
+        return favouritesList.map(trip => (<FilteredQueryResults
                 trip={trip}
                 key={trip.id}
                 // handleFavIcon={}
+                setFavouriteTrip={setFavouriteTrip}
                 setSelectedTrip={setSelectedTrip}
                 favourites={favourites}
                 defaultImg={defaultImg}
@@ -74,7 +100,7 @@ const Favourites2 = () => {
 };
 
 
-class Favourites extends Component {
+class Favourites2 extends Component {
     state = {
         results: [],
         selectedTrip: null,
