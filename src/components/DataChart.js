2958446f5211ac7403deea9ae32087e5a081053a
rednaxela700 <ohleg@windowslive.com>
2020-10-04 20:42:03 +0200

    src/components/DataChart.js: converted to functional component

--- src/components/DataChart.js
+++ src/components/DataChart.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BarChart,
   Bar,
@@ -11,58 +11,53 @@ import {
 import { fetchUsers } from "../services/TripService";
 
 const windowWidth = window.screen.width;
-export default class DataBarChart extends PureComponent {
-  state = {
-    barChartData: []
-  }
 
-  componentDidMount() {
-    const usersData = fetchUsers();
-    usersData.then(res => this.setState({
-      barChartData: res
-    }))
-  }
+const DataBarChart = () => {
+  const [barChartData, setBarchartData] = useState([])
+  useEffect(() => {
+    const f = async () => {
+      const result = await fetchUsers()
+      setBarchartData(result)
+    }
+    f()
+    // eslint-disable-next-line
+  }, [])
 
+  const mockData = [
 
-  get data() {
+    {
+      name: 'Listopad',
+      mobileName: '08',
+      uv: 2,
+      pv: 2,
+      amt: 2,
+    },
+    {
+      name: 'Grudzień',
+      mobileName: '10',
+      uv: barChartData.length,
+      pv: 3908,
+      amt: 2000,
+    }
+  ]
 
-    return [
-
-      {
-        name: 'Listopad',
-        mobileName: '08',
-        uv: 2,
-        pv: 2,
-        amt: 2,
-      },
-      {
-        name: 'Grudzień',
-        mobileName: '10',
-        uv: this.state.barChartData.length,
-        pv: 3908,
-        amt: 2000,
-      }
-    ]
-
-  }
-
-  render() {
-    return (<div>
-      <BarChart
-        width={windowWidth > 500 ? 500 : 300}
-        height={windowWidth > 500 ? 300 : 150}
-        data={this.data}
-        style={{ margin: '0 auto' }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey={windowWidth > 500 ? "name" : "mobileName"} />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        {/*<Bar dataKey="pv" fill="#8884d8" />*/}
-        <Bar dataKey="uv" fill="#82ca9d" name={'Liczba zarejestrowanych użytkowników'} />
-      </BarChart>
-    </div>
-    );
-  }
+  return (<div>
+    <BarChart
+      width={windowWidth > 500 ? 500 : 300}
+      height={windowWidth > 500 ? 300 : 150}
+      data={mockData}
+      style={{ margin: '0 auto' }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey={windowWidth > 500 ? "name" : "mobileName"} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      {/*<Bar dataKey="pv" fill="#8884d8" />*/}
+      <Bar dataKey="uv" fill="#82ca9d" name={'Liczba zarejestrowanych użytkowników'} />
+    </BarChart>
+  </div>
+  );
 }
+
+export default DataBarChart
