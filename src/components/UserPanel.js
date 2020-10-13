cb82b340524c9b39d822d7a631bfc26284780de3
rednaxela700 <ohleg@windowslive.com>
2020-10-13 22:37:29 +0200

    src/components/UserPanel.js: added fn fetching current user data

--- src/components/UserPanel.js
+++ src/components/UserPanel.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Header } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
 import firebase from "../firebase";
 
 function UserPanel() {
+
+  const fetchUser = async () => {
+    const userId = firebase.auth().currentUser.uid;
+    const fetched = await firebase.database().ref('/users/' + userId).once('value')
+    const user = fetched.val()
+    console.log(user)
+  }
+  fetchUser()
+
   return (
     <div>
       <Header>dane z firebase dla usera {firebase.auth().currentUser.uid} </Header>
